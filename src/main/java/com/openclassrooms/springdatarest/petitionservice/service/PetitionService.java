5e379d625d11fcb5f78878c8f42e0afbb4fd7286
Raf Gemmail <rafiq@dreamthought.com>
2020-01-12 14:45:15 +1300

    Added a petition service

--- src/main/java/com/openclassrooms/springdatarest/petitionservice/service/PetitionService.java
+++ src/main/java/com/openclassrooms/springdatarest/petitionservice/service/PetitionService.java
@@ -1,7 +1,17 @@
 package com.openclassrooms.springdatarest.petitionservice.service;
 
+import com.openclassrooms.springdatarest.petitionservice.domain.Petition;
+import com.openclassrooms.springdatarest.petitionservice.repository.PetitionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PetitionService {
+
+    @Autowired
+    private PetitionRepository petitionRepository;
+
+    public Iterable<Petition> listAllPetitions() {
+        return petitionRepository.findAll();
+    }
 }
