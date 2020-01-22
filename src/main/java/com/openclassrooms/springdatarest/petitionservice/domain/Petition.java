ca8d978c370394ed180f6a1e419fb3f6b2ab07c1
Raf Gemmail <rafiq@dreamthought.com>
2020-01-23 07:51:48 +1300

    Added @GeneratedValue and id getter

--- src/main/java/com/openclassrooms/springdatarest/petitionservice/domain/Petition.java
+++ src/main/java/com/openclassrooms/springdatarest/petitionservice/domain/Petition.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.springdatarest.petitionservice.domain;
 
+import javax.annotation.processing.Generated;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
@@ -10,14 +12,18 @@ import javax.persistence.Id;
 public class Petition {
 
     @Id
+    @GeneratedValue
     private long id;
 
     private String title;
 
+    public long getId() {
+        return id;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getTitle() {
         return title;
     }
