b0283d73e4b953cb2bb43cee981f67cefbf69af9
Raf Gemmail <rafiq@dreamthought.com>
2020-01-25 11:10:29 +1300

    Added an Activist entity

--- src/main/java/com/openclassrooms/springdatarest/petitionservice/domain/Activist.java
+++ src/main/java/com/openclassrooms/springdatarest/petitionservice/domain/Activist.java
@@ -1,4 +1,50 @@
 package com.openclassrooms.springdatarest.petitionservice.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Activist {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String address;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
