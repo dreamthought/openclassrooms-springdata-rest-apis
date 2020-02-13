079ac9429201d086117a94121ee243ad910a2223
Raf Gemmail <rafiq@dreamthought.com>
2020-02-14 08:29:08 +1300

    Added example to only expose PUT, POST and PATCH

--- src/main/java/com/openclassrooms/springdatarest/petitionservice/repository/PetitionRepository.java
+++ src/main/java/com/openclassrooms/springdatarest/petitionservice/repository/PetitionRepository.java
@@ -1,8 +1,14 @@
 package com.openclassrooms.springdatarest.petitionservice.repository;
+
 import com.openclassrooms.springdatarest.petitionservice.domain.Petition;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource(collectionResourceRel = "petitions", path = "petitions")
+@RepositoryRestResource(collectionResourceRel = "patch-petitions", path = "patch-petitions")
 public interface PetitionRepository extends CrudRepository<Petition, Long> {
+
+    @RestResource(exported = true)
+    @Override
+    <S extends Petition> S save(S s);
 }
