2d9f9dcab38abfcb9b11c5062e780a7399d11569
Raf Gemmail <rafiq@dreamthought.com>
2020-02-16 11:02:49 +1300

    Added spring data rest config for petitions

---
+++ src/main/java/com/openclassrooms/springdatarest/petitionservice/SpringDataRestConfig.java
@@ -0,0 +1,28 @@
+package com.openclassrooms.springdatarest.petitionservice;
+
+import com.openclassrooms.springdatarest.petitionservice.domain.Petition;
+import com.openclassrooms.springdatarest.petitionservice.domain.Signature;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class SpringDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.getExposureConfiguration()
+                // Specify entity
+                .forDomainType(Petition.class)
+                // Provide a lambda to disable HTTP Methods for single entities
+                .withItemExposure((metadata, itemHttpMethods) ->
+                        itemHttpMethods.disable(
+                                HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE))
+                // Provide a lambda to disable HTTP Methods for collections
+                .withCollectionExposure((collectionMetdata, collectionHttpMethods) ->
+                        collectionHttpMethods.disable(
+                                HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE));
+
+    }
+}
