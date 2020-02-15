09885a9e06e0f058e490a33df249c2be8017cebb
Raf Gemmail <rafiq@dreamthought.com>
2020-02-15 22:43:16 +1300

    Add methods to block

--- src/main/java/com/openclassrooms/springdatarest/petitionservice/repository/PetitionRepository.java
+++ src/main/java/com/openclassrooms/springdatarest/petitionservice/repository/PetitionRepository.java
@@ -5,10 +5,21 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.Optional;
+
+
 @RepositoryRestResource(collectionResourceRel = "patch-petitions", path = "patch-petitions")
 public interface PetitionRepository extends CrudRepository<Petition, Long> {
 
     @RestResource(exported = true)
     @Override
     <S extends Petition> S save(S s);
+
+    @RestResource(exported = false)
+    @Override
+    Optional<Petition> findById(Long aLong);
+
+    @RestResource(exported = false)
+    @Override
+    Iterable<Petition> findAll();
 }
