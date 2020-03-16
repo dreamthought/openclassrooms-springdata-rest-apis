1ff9900363210235cf5c878e58eb054d847bed66
Raf Gemmail <rafiq@dreamthought.com>
2020-03-16 21:44:11 +1300

    Added transactional to PetitionService

--- src/main/java/com/openclassrooms/springdatarest/petitionservice/service/PetitionService.java
+++ src/main/java/com/openclassrooms/springdatarest/petitionservice/service/PetitionService.java
@@ -8,12 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class PetitionService {
 
     private static Logger LOGGER = LoggerFactory.getLogger(PetitionService.class.getName());
