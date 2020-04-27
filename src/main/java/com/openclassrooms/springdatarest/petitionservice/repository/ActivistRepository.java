62ec1ea02284b60385636430f12be1ab9956ba4e
Raf Gemmail <rafiq@dreamthought.com>
2020-04-28 08:57:03 +1200

    Added a paginatinated GET /activists

--- src/main/java/com/openclassrooms/springdatarest/petitionservice/repository/ActivistRepository.java
+++ src/main/java/com/openclassrooms/springdatarest/petitionservice/repository/ActivistRepository.java
@@ -1,7 +1,11 @@
 package com.openclassrooms.springdatarest.petitionservice.repository;
 
 import com.openclassrooms.springdatarest.petitionservice.domain.Activist;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface ActivistRepository extends JpaRepository<Activist, Long> {
+public interface ActivistRepository extends PagingAndSortingRepository<Activist, Long> {
+    public Page<Activist> findAll(Pageable p);
 }
